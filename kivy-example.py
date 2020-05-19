7130670b2be8db78a8165c1017579728f2cc89fb
TXC4 <43871212+TXC4@users.noreply.github.com>
2020-05-19 02:47:40 -0500

    Add files via upload

--- kivy-example.py
+++ kivy-example.py
@@ -12,6 +12,7 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.widget import Widget
+from kivy.graphics import Rectangle, Color
 import random
 
 Builder.load_string("""
@@ -21,35 +22,59 @@ Builder.load_string("""
 <NumbersScreen>:
     GridLayout:
         rows: 2
+        size_hint: 1, .85
+        pos_hint: {"top" : .85}
+        Label:
+            id: timeLabel
+            text: root.timeNum
+            size_hint_y: .1
         ScrollView:
             do_scroll_x: False
             do_scroll_y: True
             Label:
                 id: numLabel
-                text: "Number"
+                text: "Press Start to begin.\\n\\n-Generates random digits to practice memorization.\\n-You may customize quantity, group, and base."
+                font_size: "20sp"
                 size_hint_y: None
                 text_size: self.width, None
                 height: self.texture_size[1]
                 padding_x: 10
-        Label:
-            id: timeLabel
-            text: root.timeNum
+                padding_y: 10
+        
     GridLayout:
-        cols: 2
-        size_hint: 1, .2
+        cols: 3
+        size_hint: 1, .15
+        pos_hint: {"top" : 1}
         Button:
-            text: "New Number"
-            on_press: root.getNumbers(2, 1000)
-            on_press: root.startTime()
-            on_press: root.timeInt = 0
+            text: "Start/Reset"
+            on_release: root.getNumbers(2, 1000)
+            on_release: root.startTime()
+            on_release: root.timeInt = 0
         Button:
             text: "Stop"
-            on_press: root.stopTime()
+            on_release: root.stopTime()
+        Button:
+            text: "Customize"
+    
 <CardsScreen>:
+    canvas.before:
+        Color:
+            rgba: .14,.40,.20,1
+        Rectangle:
+            pos: app.pos
+            size: app.size
     GridLayout:
-        cols: 1
+        rows:1
+        size_hint: 1,1
         Label:
-            text: "Screen 2"
+            text: "Press Shuffle to begin.\\n-Swipe right for next card\\n-Swipe left for previous card\\n-You may customize the number of cards or number of decks."
+            font_size: "20sp"
+            size_hint_y: None
+            text_size: self.width, None
+            height: self.texture_size[1]
+            padding_x: 10
+            padding_y: 10  
+
 <Screen3>:
     Label:
         text: "Screen 3"
@@ -61,29 +86,29 @@ Builder.load_string("""
         text: "Screen 5"
 <MyApp>:
     rows: 2
-    size_hint: 1, .9
     ScreenSwitcher:
         id: manager
-
+        size_hint: 1, .9
+        pos_hint: {"top" : 1}
     GridLayout:
         cols: 5
         size_hint: 1, .1
         Button:
-            text: "1"
+            text: "1234\\n1010"
             #'ns' is declared in python ScreenSwitcher class
-            on_press: root.ids.manager.current = 'ns'
+            on_release: root.ids.manager.current = 'ns'
         Button:
             text: "2"
-            on_press: root.ids.manager.current = 'cs'
+            on_release: root.ids.manager.current = 'cs'
         Button:
             text: "3"
-            on_press: root.ids.manager.current = 's3'
+            on_release: root.ids.manager.current = 's3'
         Button:
             text: "4"
-            on_press: root.ids.manager.current = 's4'
+            on_release: root.ids.manager.current = 's4'
         Button:
             text: "5"
-            on_press: root.ids.manager.current = 's5'
+            on_release: root.ids.manager.current = 's5'
 """)
 class ScreenSwitcher(ScreenManager):
     def __init__(self,**kwargs):
