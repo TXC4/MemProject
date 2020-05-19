be4a3e0c778a0ee589d919d51d92be8ecd975f7c
TXC4 <43871212+TXC4@users.noreply.github.com>
2020-05-19 13:16:12 -0500

    Add files via upload

--- kivy-example.py
+++ kivy-example.py
@@ -5,8 +5,11 @@ from kivy.app import App
 from kivy.clock import Clock
 from kivy.lang import Builder
 from kivy.properties import StringProperty, NumericProperty
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition, WipeTransition
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.checkbox import CheckBox
+from kivy.uix.dropdown import DropDown
+from kivy.uix.spinner import Spinner
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
@@ -35,6 +38,7 @@ Builder.load_string("""
                 id: numLabel
                 text: "Press Start to begin.\\n\\n-Generates random digits to practice memorization.\\n-You may customize quantity, group, and base."
                 font_size: "20sp"
+                halign: "center"
                 size_hint_y: None
                 text_size: self.width, None
                 height: self.texture_size[1]
@@ -42,19 +46,35 @@ Builder.load_string("""
                 padding_y: 10
         
     GridLayout:
-        cols: 3
+        cols: 5
         size_hint: 1, .15
         pos_hint: {"top" : 1}
         Button:
-            text: "Start/Reset"
-            on_release: root.getNumbers(2, 1000)
+            font_size: "14sp"
+            text: "Start\\nReset"
+            on_release: root.newNums()
             on_release: root.startTime()
             on_release: root.timeInt = 0
         Button:
+            font_size: "14sp"
             text: "Stop"
             on_release: root.stopTime()
-        Button:
-            text: "Customize"
+        Spinner:
+            font_size: "14sp"
+            text: "Quantity"
+            values: ('3','5','10','15','20','25','50','100','250','500','1000','2500','5000','10000')
+            on_text: root.printv(self.text), root.setQuantity(self.text)
+        Spinner:
+            font_size: "14sp"
+            text: "Grouping"
+            values: ('None','1', '2','3','4','5','10')
+            on_text: root.printv(self.text), root.setGrouping(self.text)
+        Spinner:
+            font_size: "14sp"
+            text: "Base"
+            values: ('Binary', 'Base 10', 'Hex')
+            on_text: root.printv(self.text)
+                
     
 <CardsScreen>:
     canvas.before:
@@ -94,11 +114,13 @@ Builder.load_string("""
         cols: 5
         size_hint: 1, .1
         Button:
-            text: "1234\\n1010"
+            font_size: "12sp"
+            text: "numbers"
             #'ns' is declared in python ScreenSwitcher class
             on_release: root.ids.manager.current = 'ns'
         Button:
-            text: "2"
+            font_size: "12sp"
+            text: "cards"
             on_release: root.ids.manager.current = 'cs'
         Button:
             text: "3"
@@ -110,9 +132,15 @@ Builder.load_string("""
             text: "5"
             on_release: root.ids.manager.current = 's5'
 """)
+
+
+########################################################################################
+########################################################################################
+########################################################################################
+
 class ScreenSwitcher(ScreenManager):
     def __init__(self,**kwargs):
-        super(ScreenSwitcher, self).__init__(**kwargs)
+        super(ScreenSwitcher, self).__init__(**kwargs,  transition=WipeTransition())
         self.add_widget(HomeScreen(name='hs'))
         self.add_widget(NumbersScreen(name='ns'))
         self.add_widget(CardsScreen(name='cs'))
@@ -122,8 +150,22 @@ class ScreenSwitcher(ScreenManager):
 class HomeScreen(Screen):
     pass
 class NumbersScreen(Screen, GridLayout):
+    quantity = 25
+    grouping = 0
+    base = 10
     timeNum = StringProperty()
     timeInt = 0
+    def printv(self, x):
+        print(x)
+    def setQuantity(self, quantity):
+        self.quantity = int(quantity)
+    def setGrouping(self, grouping):
+        if grouping == 'None':
+            self.grouping = 0
+        else:
+            self.grouping = int(grouping)
+    def setBase(self, base):
+        self.base = int(base)
     def increment_time(self, interval):
         if (self.timeInt == 0):
             self.timeNum = '0'
@@ -142,11 +184,17 @@ class NumbersScreen(Screen, GridLayout):
         return str(x)
     def getNumbers(self, groupSize, quantity):
         x = ""
-        for i in range(quantity):
-            if (i != 0) & ((i % groupSize) == 0):
-                x += " "
-            x += self.getRand()
+        if groupSize == 0:
+            for i in range(quantity):
+                x += self.getRand()
+        else:
+            for i in range(quantity):
+                if (i != 0) & ((i % groupSize) == 0):
+                    x += " "
+                x += self.getRand()
         self.ids.numLabel.text = x
+    def newNums(self):
+        self.getNumbers(self.grouping, self.quantity)
         
 class CardsScreen(Screen, GridLayout):
     pass
